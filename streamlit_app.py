6d63e8cfeb30655f18f294704332a639065a607d
mohammedchtioui <54271352+mohammedchtioui@users.noreply.github.com>
2023-02-16 15:56:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
-streamlit.title('my parent new healty diner')
-streamlit.header('breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kali , spinach & Rocket Smoothie')
-streamlit.text('OHard-Boiled Free range Egg')
+streamlit.title(' my parent new healty diner')
+streamlit.header('  breakfast menu')
+streamlit.text(' 🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kali , spinach & Rocket Smoothie')
+streamlit.text('🐔 OHard-Boiled Free range Egg')
+streamlit.text('🥑🍞 Avocado toast')
 
 
